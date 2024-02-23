4a53afab5dd3637b6a85cc787e3b557359e0b78d
Pankaj sharam <Pankajsharma1997>
2024-02-23 11:10:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const chalk = require('chalk');
+
+// const log = console.log;
+
+// // Combine styled and normal strings
+// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+const app = require('colors')
+console.log("helo".green);
+
+
